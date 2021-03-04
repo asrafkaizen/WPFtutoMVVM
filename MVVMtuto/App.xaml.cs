8fb55cc2051dbf6f0c14544bf753f0c01d5c6295
asyraf <asrafkaizen@gmail.com>
2021-03-04 15:37:52 +0800

    note that update btn is useless actually

---
+++ MVVMtuto/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using MVVMtuto.View;
+using MVVMtuto.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVMtuto
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            MainWindow window = new MainWindow();
+            UserViewModel VM = new UserViewModel();
+            window.DataContext = VM;
+            window.Show();
+        }
+    }
+}
